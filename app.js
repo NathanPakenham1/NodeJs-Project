f0f4712ec68a17e4b1934612c85573b2f5750d16
nathan <97966664+njpwastaken1@users.noreply.github.com>
2024-03-21 12:10:16 +0000

    start setup

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+
+const app = express();
+const port = process.env.PORT || 4000; // Adjusted port definition
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static Files
+app.use(express.static('public'));
+
+// Templating Engine
+app.use(expressLayout);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
+
+// Home
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
