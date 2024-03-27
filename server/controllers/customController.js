232c413e8d90c2babb1451a40b2290a5ea6ab2fd
njpwastaken <97966664+njpwastaken1@users.noreply.github.com>
2024-03-27 10:54:57 +0000

    Project Video Done

--- server/controllers/customController.js
+++ server/controllers/customController.js
@@ -23,26 +23,54 @@ const mongoose = require('mongoose');
 //     }
 // };
 
-
-exports.homepage = async (req, res) => {
-    const message = await req.flash('info'); // Retrieve flash m
-
-    const locals = {
-        title: 'Nodejs',
-        description: 'Free Nodejs User Management System'
-    };
-
-    let perPage = 12;
-    let page = req.query.page || 1;
+exports.about = async (req, res) => {
+  const locals = {
+    title: 'About',
+    description: 'Free Nodejs User Management System'
+};
 
     try {
-     const customers = await
+        res.render('about', locals); // Pass messages to the view
     } catch (error) {
         console.error(error);
-        req.flash('error', 'An error occurred while loading the homepage.'); // Flash error message
-        res.redirect('/'); // Redirect to homepage or handle error appropriately
     }
 };
+
+
+exports.homepage = async (req, res) => {
+    // Remove
+    // const messages = await req.consumeFlash('info');
+    // Use this instead
+    const messages = await req.flash("info");
+  
+    const locals = {
+      title: "NodeJs",
+      description: "Free NodeJs User Management System",
+    };
+   
+    let perPage = 12;
+    let page = req.query.page || 1;
+  
+    try {
+      const customers = await Customer.aggregate([{ $sort: { createdAt: -1 } }])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+      // Count is deprecated. Use countDocuments({}) or estimatedDocumentCount()
+      // const count = await Customer.count();
+      const count = await Customer.countDocuments({});
+  
+      res.render("index", {
+        locals,
+        customers,
+        current: page,
+        pages: Math.ceil(count / perPage),
+        messages,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
 // Get New Customer
 
@@ -78,5 +106,115 @@ exports.postCustomer = async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-
 }
+
+exports.view = async (req, res) => {
+    try {
+        const customer = await Customer.findOne({ _id: req.params.id });
+
+        const locals = {
+            title: "View Customer Data",
+            description: "Free NodeJs User Management System",
+        };
+
+        res.render('customer/view', {
+            locals,
+            customer
+        });
+    }
+    catch(error) {
+        console.log(error);
+        // Handle the error response if necessary
+        res.status(500).send("An error occurred while processing your request.");
+    }
+};
+
+// Edit Customer Data
+
+exports.edit = async (req, res) => {
+    try {
+        const customer = await Customer.findOne({ _id: req.params.id });
+
+        const locals = {
+            title: "Edit Customer Data",
+            description: "Free NodeJs User Management System",
+        };
+
+        res.render('customer/edit', {
+            locals,
+            customer
+        });
+    }
+    catch(error) {
+        console.log(error);
+        // Handle the error response if necessary
+        res.status(500).send("An error occurred while processing your request.");
+    }
+};
+
+// Update Customer Data
+
+exports.editPost = async (req, res) => {
+    try {
+      await Customer.findByIdAndUpdate(req.params.id, {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        tel: req.body.tel,
+        email: req.body.email,
+        details: req.body.details,
+        updatedAt: Date.now(),
+      });
+      await res.redirect(`/edit/${req.params.id}`);
+  
+      console.log("redirected");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+// Delete Customer Data
+
+exports.deleteCustomer = async (req, res) => {
+    try {
+        try {
+          await Customer.deleteOne({ _id: req.params.id });
+          res.redirect("/");
+        } catch (error) {
+          console.log(error);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+  };
+
+
+// Search Customer 
+
+exports.searchCustomers = async (req, res) => {
+
+  const locals = {
+    title: "Search Customer Data",
+    description: "Free NodeJs User Management System",
+  };
+ 
+
+  try {
+    let searchTerm = req.body.searchTerm;
+    const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+    const customers = await Customer.find({
+      $or: [
+        { firstName: {$regex: new RegExp(searchNoSpecialChar, "i")} },
+        { lastName: {$regex: new RegExp(searchNoSpecialChar, "i")} },
+      ]
+    });
+
+    res.render("search", {
+      customers,
+      locals
+    })
+
+  } catch (error) {
+    console.log(error);
+  }
+};
--- server/routes/customer.js
+++ server/routes/customer.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const customerController = require('../controllers/customController')
 
 router.get('/', customerController.homepage);
+router.get('/about', customerController.about);
+
 router.get('/add', customerController.addCustomer);
 router.post('/add', customerController.postCustomer);
+router.get('/view/:id', customerController.view);
+
+router.get('/edit/:id', customerController.edit);
+router.put('/edit/:id', customerController.editPost);
+router.delete('/edit/:id', customerController.deleteCustomer);
+
+router.post('/search', customerController.searchCustomers);
+
 
 
 module.exports = router;
